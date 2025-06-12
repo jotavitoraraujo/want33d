bbbe9efc3f600bcf91eda50854df0179a8fa8d65
João Vitor <vitor.oliveira015@gmail.com>
2025-06-12 01:39:03 -0300

    Refatoração completa: separação entre preco.py e tvl.py, atualização do README.md com estrutura atualizada e registro das mudanças de 12/06/2025

--- tvl.py
+++ tvl.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timezone, timedelta
 # função personalizada criada p/ consulta do tvl atual
 def tvl_aero():
     url_tvl_aero = 'https://api.llama.fi/tvl/aerodrome'
@@ -8,16 +9,24 @@ aero_tvl = tvl_aero()
 print(f"TVL atual da AERO: ${aero_tvl:,.2f}")
 # função personalizada criada p/ consulta do tvl dos ultimos 30d
 def tvl_30d():
-    url_tvl30d = 'https://api.llama.fi/chart/protocol/aerodrome'
+    url_tvl30d = 'https://api.llama.fi/protocol/aerodrome-v1'
     resposta_tvl30d = requests.get(url_tvl30d)
     if resposta_tvl30d.status_code == 200:
         tvl30dias = resposta_tvl30d.json()
-        tvl_passado = tvl30dias[-31]['tvl']
-        return tvl_passado
+        timestamp_30d_exatos = int((datetime.now(timezone.utc) - timedelta(days=30)).timestamp())
+        registro_ideal = None
+        menor_diferenca = float('inf')
+        for item in tvl30dias['tvl']:
+            diferenca = abs(item['date'] - timestamp_30d_exatos)
+            if diferenca < menor_diferenca:
+                registro_ideal = item
+        tvl_passado = registro_ideal['totalLiquidityUSD']
+        data_passada = datetime.fromtimestamp(registro_ideal['date'], tz=timezone.utc).strftime('%d/%m/%Y')
+        return tvl_passado, data_passada
     else:
         print('Erro ao acessar o historico de TVL. Codigo:', resposta_tvl30d.status_code)
         return None
-tvl_30d_atras = tvl_30d()
+tvl_30d_atras, data_passada = tvl_30d()
 # logica condicional para analise do tvl
 if tvl_30d_atras is not None:
     if tvl_30d_atras > aero_tvl:
@@ -28,4 +37,4 @@ if tvl_30d_atras is not None:
         resultado = 'tvl estavel'
 else: 
     resultado = 'Erro ao obter TVL de 30 dias atrás'
-print(f'Resultado da análise de TVL: {resultado}')
+print(f'Resultado da análise de TVL de 30 dias atrás ({data_passada}) para data atual: {resultado}')
